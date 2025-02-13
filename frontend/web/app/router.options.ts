5f903cc31e6e803fe8aa9dadc9c6cb40aaa9dd06
Vadim SkinonikS.# <skinnoniks@gmail.com>
2025-02-13 17:43:53 +0300

    another router configuration fix

--- frontend/web/app/router.options.ts
+++ frontend/web/app/router.options.ts
@@ -5,14 +5,14 @@ import type { LocationQuery } from 'vue-router';
 export default {
   parseQuery: (query) => parse(query, {
     arrayLimit: 100,
-    comma: true,
     parameterLimit: 100,
+    ignoreQueryPrefix: true,
     duplicates: 'combine',
   }) as LocationQuery,
   stringifyQuery: (query) => stringify(query, {
     skipNulls: true,
     allowEmptyArrays: false,
-    arrayFormat: 'comma',
-    commaRoundTrip: true,
+    arrayFormat: 'brackets',
+    addQueryPrefix: false,
   }),
 } satisfies RouterConfig;
