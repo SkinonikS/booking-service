0a52135997c20c7b93904cd766ae52ecdcdfa49c
Vadim SkinonikS.# <skinnoniks@gmail.com>
2025-04-03 18:58:32 +0300

    add DeleteBookingProviderGalleryImageMutation for deleting gallery images

---
+++ backend/api/app/GraphQL/Mutations/DeleteBookingProviderGalleryImageMutation.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\BookingProvider;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+final readonly class DeleteBookingProviderGalleryImageMutation
+{
+    use AuthorizesRequests;
+
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args)
+    {
+        $bookingProvider = BookingProvider::query()->findOrFail($args['id']);
+
+        $this->authorize('delete', $bookingProvider);
+
+        $coverImage = $bookingProvider->media()
+            ->where('tag', 'gallery')
+            ->where('id', $args['id'])
+            ->first();
+
+        if ($coverImage) {
+            $bookingProvider->detachMedia($coverImage);
+        }
+
+        return $coverImage;
+    }
+}
