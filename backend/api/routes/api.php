a849d30f69944ba88ea8f78090d2bf625f86f3e5
Vadim SkinonikS.# <skinnoniks@gmail.com>
2025-04-03 18:13:21 +0300

    add routes for uploading cover and gallery images in BookingProvider

--- backend/api/routes/api.php
+++ backend/api/routes/api.php
@@ -1,15 +1,10 @@
 <?php
 
-// use Illuminate\Http\Request;
-
-use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Api\V1\BookingProviderCoverController;
+use App\Http\Controllers\Api\V1\BookingProviderGalleryController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/v1')->group(function () {
-    Route::get('/me/profile', function () {
-        return Response::json([
-            'data' => Auth::user(),
-        ]);
-    });
+    Route::post('/booking-providers/{booking_provider}/cover-image/upload', BookingProviderCoverController::class)->middleware('auth:cognito');
+    Route::post('/booking-providers/{booking_provider}/gallery-images/upload', BookingProviderGalleryController::class)->middleware('auth:cognito');
 });
