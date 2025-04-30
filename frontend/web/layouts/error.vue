4ccce86beb271814865af87387b9361c2edf5d68
Vadim SkinonikS.# <skinnoniks@gmail.com>
2025-04-30 20:46:29 +0300

    fix component name

--- frontend/web/layouts/error.vue
+++ frontend/web/layouts/error.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid grid-rows-[1fr] h-dvh">
-    <PageMain class="row-span-1">
+    <BasePageMain class="row-span-1">
       <slot />
-    </PageMain>
+    </BasePageMain>
   </div>
 </template>
